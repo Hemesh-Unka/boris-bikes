ea49f64fd26fd55fa5fe2a9fc161190097d03d9b
Vivian Allen <mrvivianallen@gmail.com>
2018-02-23 16:23:15 +0000

    exercise 20 (pretty much) done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -16,6 +16,11 @@ class DockingStation
     @bikes.delete(return_working_bikes[0])
   end
 
+  def release_broken_bike
+    fail 'No broken bikes available' if no_broken?
+    @bikes.delete(return_broken_bikes[0])
+  end
+
   def dock_bike(bike)
     fail 'Not enough space available' if full?
     @bikes.push(bike)[-1]
@@ -27,6 +32,11 @@ class DockingStation
     false
   end
 
+  def no_broken?
+    return true if return_broken_bikes.empty?
+    false
+  end
+
   def full?
     @bikes.length >= @capacity
   end
@@ -34,4 +44,9 @@ class DockingStation
   def return_working_bikes
     @bikes.select { |bike| bike.working == true }
   end
+
+  def return_broken_bikes
+    @bikes.select { |bike| bike.working == false }
+  end
+
 end
